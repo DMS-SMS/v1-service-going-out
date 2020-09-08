f21c2779f679ea439e6bc307b6c1aa429ea54874
Maly <migsking@naver.com>
2020-09-09 01:34:12 +0900

    [CREATE] <*> 구조 init

---
+++ application/config/app_config.py
@@ -0,0 +1,7 @@
+class DevLevelConfig:
+    worker_node = 10
+
+    host = "0.0.0.0"
+    port = 50051
+
+    _address = str(host + ":" + str(port))
---
+++ presentation/__init__.py
@@ -0,0 +1,16 @@
+import grpc
+
+
+def register_hooks():
+    pass
+
+
+def register_presentations(app):
+    pass
+
+
+def create_app():
+    app = grpc
+
+    register_presentations(app)
+    return app
