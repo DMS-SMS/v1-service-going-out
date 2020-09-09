f04af8cc084b5f8dc721ac9c31b272750fcd8b42
Maly <migsking@naver.com>
2020-09-09 10:53:02 +0900

    [MODIFY] <run.py>

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from presentation import create_app
 
 
-if __name__ == '__main__':
+def serve():
     app = create_app()
     app.start()
     app.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
