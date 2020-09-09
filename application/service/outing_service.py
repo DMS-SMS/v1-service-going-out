2a211409df0f997880a0e955a5a861fcb7d7dc6c
Maly <migsking@naver.com>
2020-09-09 19:31:10 +0900

    [ADD] <application/service/outing_service.py> Create the "create outing"

---
+++ application/service/outing_service.py
@@ -0,0 +1,25 @@
+from proto.python.outing import outing_student_pb2
+
+from application.mapper import create_outing_mapper
+from application.exception import OutingExist
+
+from domain.repository.outing_repository import OutingRepository
+from domain.entity.outing import Outing
+
+from infrastructure.repository.outing_repository_impl import OutingRepositoryImpl
+
+
+class OutingService:
+    @classmethod
+    def create_outing(cls, request):
+        repository: OutingRepository = OutingRepositoryImpl()
+        entity: Outing = create_outing_mapper(request)
+
+        response = outing_student_pb2.CreateOutingResponse
+
+        try:
+            oid = repository.save(entity)
+        except OutingExist as e:
+            return response(status=e.status, code=e.code, msg=e.msg)
+
+        return response(status=201, msg="Created", oid=oid)
