24a29950df2492a37b9c536201d3979a94d0655d
Maly <migsking@naver.com>
2020-09-09 10:22:53 +0900

    [CREATE] <presentation/config> Create config

---
+++ presentation/config/__init__.py
@@ -0,0 +1,14 @@
+class gRPCAppConfig:
+    def __init__(self):
+        self._max_workers = 10
+        self._host = "0.0.0.0"
+        self._port = 50051
+
+    @property
+    def max_workers(self):
+        return self._max_workers
+
+    @property
+    def address(self):
+        return str(self._host+":"+str(self._port))
+
