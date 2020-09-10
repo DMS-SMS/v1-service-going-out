e9a2aba5271ad66c1338458b7ad5e5c861b49016
Maly <migsking@naver.com>
2020-09-10 15:34:02 +0900

    [MODIFY] <application/mapper/create_outing_mapper.py>

--- application/mapper/create_outing_mapper.py
+++ application/mapper/create_outing_mapper.py
@@ -3,16 +3,11 @@ from domain.entity.outing import Outing
 
 def create_outing_mapper(request):
     entity: Outing = Outing(
-        outing_uuid=None,
         student_uuid=request.uuid,
-        status=None,
         situation=request.situation,
-        accept_teacher=None,
         date=request.date,
         start_time=request.start_time,
         end_time=request.end_time,
         place=request.place,
-        reason=request.reason,
-        arrival_date=None,
-        arrival_time=None)
+        reason=request.reason)
     return entity
