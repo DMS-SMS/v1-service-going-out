a8a21792606a44d95aabc233562508736f804977
Maly <migsking@naver.com>
2020-09-10 19:51:51 +0900

    [CREATE] <infra../config> Redis Config 추가

--- infrastructure/config/__init__.py
+++ infrastructure/config/__init__.py
@@ -21,3 +21,26 @@ class DatabaseConfig:
     def autoflush(self):
         return self._autoflush
 
+
+class RedisConfig:
+    def __init__(self):
+        self._host: str = "127.0.0.1"
+        self._port: int = 6379
+        self._password: Optional[str] = None
+        self._db: int = 1
+
+    @property
+    def host(self):
+        return self._host
+
+    @property
+    def port(self):
+        return self._port
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def db(self):
+        return self._db
