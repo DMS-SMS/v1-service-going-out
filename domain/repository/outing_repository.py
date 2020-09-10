f38dfbc21f9b41901e972e31e75adb1c87f78a59
Maly <migsking@naver.com>
2020-09-10 19:54:09 +0900

    [ADD] <domain/repository/outing_repository.py> outingcode 관련 레포지토리 추가

--- domain/repository/outing_repository.py
+++ domain/repository/outing_repository.py
@@ -6,4 +6,8 @@ class OutingRepository(metaclass=ABCMeta):
     @classmethod
     @abstractmethod
     def save_and_get_oid(cls, outing: Outing) -> int:
+
+    @classmethod
+    @abstractmethod
+    def set_and_get_parents_outing_code(cls, oid) -> str:
         pass
