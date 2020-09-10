a411c999ae93327ed69238c4503223e5e52e0b33
Maly <migsking@naver.com>
2020-09-10 19:51:19 +0900

    [ADD] <infra../extension> redis 익스텐션 추가

--- infrastructure/extension/__init__.py
+++ infrastructure/extension/__init__.py
@@ -1,4 +1,6 @@
 from infrastructure.extension.db import register_db
+from infrastructure.extension.redis_register import register_redis
 
 
 db_session, Base, engine = register_db()
+redis = register_redis()
