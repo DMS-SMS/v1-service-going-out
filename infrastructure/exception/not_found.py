4e980d95eafb995901116c3ccf6b6be569e5130e
Maly <migsking@naver.com>
2020-09-11 17:32:39 +0900

    [CREATE] <infra../exception/not_found.py> Create notfound Exception

---
+++ infrastructure/exception/not_found.py
@@ -0,0 +1,8 @@
+from infrastructure.exception.business_exception import BusinessException
+
+
+class NotFound(BusinessException):
+    def __init__(self):
+        self.status: int = 404
+        self.code: int = -1001
+        self.msg: str = "NotFound"
