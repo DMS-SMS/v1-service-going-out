d927b24ccbade99f170805b2b7683783d8e671cc
Maly <migsking@naver.com>
2020-09-11 17:33:21 +0900

    [MODIFY] <infra../exception/outing_exist.py> 비즈니스 예외 상속

--- infrastructure/exception/outing_exist.py
+++ infrastructure/exception/outing_exist.py
@@ -1,4 +1,7 @@
-class OutingExist(Exception):
+from infrastructure.exception.business_exception import BusinessException
+
+
+class OutingExist(BusinessException):
     def __init__(self):
         self.status: int = 409
         self.code: int = -1001
