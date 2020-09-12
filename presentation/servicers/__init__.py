8b1e6e6c5ea279dd621df8d61c80b94c0fe1cd29
Maly <migsking@naver.com>
2020-09-13 00:40:13 +0900

    [ADD] <presentation/servicers> Register servicer

--- presentation/servicers/__init__.py
+++ presentation/servicers/__init__.py
@@ -1,7 +1,9 @@
-from proto.python.outing import outing_student_pb2_grpc
+from proto.python.outing import outing_student_pb2_grpc, outing_teacher_pb2_grpc
 
 from presentation.servicers.outing_student_servicer import StudentOutingServicer
+from presentation.servicers.outing_teacher_servicer import TeacherOutingServicer
 
 
 def register_outing_servicers(app):
     outing_student_pb2_grpc.add_OutingStudentServicer_to_server(StudentOutingServicer(), app)
+    outing_teacher_pb2_grpc.add_OutingTeacherServicer_to_server(TeacherOutingServicer(), app)
