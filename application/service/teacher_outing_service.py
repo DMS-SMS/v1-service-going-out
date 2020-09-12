31070ec2d4535bac9324f565277314f325c72894
Maly <migsking@naver.com>
2020-09-13 00:35:02 +0900

    [CREATE] <application/service/teacher_outing_service.py>

--- application/service/teacher_outing_service.py
+++ application/service/teacher_outing_service.py
@@ -1,2 +1,18 @@
+from proto.python.outing import outing_teacher_pb2 as proto
+
+from application.decorator.error_handling import error_handling
+
+from domain.repository.outing_repository import OutingRepository
+
+from infrastructure.repository.outing_repository_impl import OutingRepositoryImpl
+
+
 class TeacherOutingService:
-    pass
+    @classmethod
+    @error_handling(proto.ConfirmOutingResponse)
+    def approve_outing(self, request):
+        repository: OutingRepository = OutingRepositoryImpl()
+
+        repository.approve_by_outing_for_teacher(request.oid)
+
+        return proto.ConfirmOutingResponse(status=200)
