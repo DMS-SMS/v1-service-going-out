141e83525162db5d03f06eed4006ebe84d360eca
Maly <migsking@naver.com>
2020-09-13 19:07:10 +0900

    [CREATE] <application/service/parents_outing_service.py>

---
+++ application/service/parents_outing_service.py
@@ -0,0 +1,18 @@
+from proto.python.outing import outing_parent_pb2 as proto
+
+from application.decorator.error_handling import error_handling
+
+from domain.repository.outing_repository import OutingRepository
+
+from infrastructure.repository.outing_repository_impl import OutingRepositoryImpl
+
+
+class ParentsOutingService:
+    @classmethod
+    @error_handling(proto.ApproveOutingByOCodeResponse)
+    def approve_outing(self, request):
+        repository: OutingRepository = OutingRepositoryImpl()
+
+        repository.approve_by_outing_for_parent(request.o_code)
+
+        return proto.ApproveOutingByOCodeResponse(status=200)
