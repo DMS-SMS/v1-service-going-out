ef627c44e13bc4a73824e231de8198d1c79774b3
Maly <migsking@naver.com>
2020-09-13 19:09:09 +0900

    [ADD] <infra../exception/not_been_approved.py> Add exapproved by teacher

--- infrastructure/exception/not_been_approved.py
+++ infrastructure/exception/not_been_approved.py
@@ -6,6 +6,15 @@ class NotApprovedByParents(BusinessException):
         self.status: int = 407
         self.code: int = -1201
         self.msg: str = "It has not been approved by parents yet"
+
+
+class AlreadyApprovedByTeacher(BusinessException):
+    def __init__(self):
+        self.status: int = 407
+        self.code: int = -1202
+        self.msg: str = "It has been approved by teacher"
+
+
 class AlreadyApprovedByParents(BusinessException):
     def __init__(self):
         self.status: int = 407
