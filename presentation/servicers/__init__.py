173217d28708cf08b621dce9806589c76a27dfcd
Maly <migsking@naver.com>
2020-09-14 18:16:21 +0900

    [MODIFY] <*> 오탈자 수정 (parent -> parents)

--- presentation/servicers/__init__.py
+++ presentation/servicers/__init__.py
@@ -1,11 +1,11 @@
-from proto.python.outing import outing_student_pb2_grpc, outing_teacher_pb2_grpc, outing_parent_pb2_grpc
+from proto.python.outing import outing_student_pb2_grpc, outing_teacher_pb2_grpc, outing_parents_pb2_grpc
 
 from presentation.servicers.outing_student_servicer import StudentOutingServicer
 from presentation.servicers.outing_teacher_servicer import TeacherOutingServicer
-from presentation.servicers.outing_parent_servicer import ParentOutingServicer
+from presentation.servicers.outing_parent_servicer import ParentsOutingServicer
 
 
 def register_outing_servicers(app):
     outing_student_pb2_grpc.add_OutingStudentServicer_to_server(StudentOutingServicer(), app)
     outing_teacher_pb2_grpc.add_OutingTeacherServicer_to_server(TeacherOutingServicer(), app)
-    outing_parent_pb2_grpc.add_OutingParentServicer_to_server(ParentOutingServicer(), app)
+    outing_parents_pb2_grpc.add_OutingParentsServicer_to_server(ParentsOutingServicer(), app)
--- presentation/servicers/outing_parent_servicer.py
+++ presentation/servicers/outing_parent_servicer.py
@@ -1,7 +1,7 @@
 from application.service.parents_outing_service import ParentsOutingService
-from proto.python.outing import outing_parent_pb2_grpc
+from proto.python.outing import outing_parents_pb2_grpc
 
 
-class ParentOutingServicer(outing_parent_pb2_grpc.OutingParentServicer):
+class ParentsOutingServicer(outing_parents_pb2_grpc.OutingParentsServicer):
     def ApproveOutingByOCode(self, request, context):
         return ParentsOutingService().approve_outing(request)
