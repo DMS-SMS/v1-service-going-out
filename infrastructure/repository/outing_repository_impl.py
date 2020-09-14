b6e59455c7bd70f9460c8c8fc1e0ee3c136568a3
Maly <migsking@naver.com>
2020-09-14 13:28:31 +0900

    [ADD] <infra../repository/outing_repository_impl.py> certify impl

--- infrastructure/repository/outing_repository_impl.py
+++ infrastructure/repository/outing_repository_impl.py
@@ -5,7 +5,7 @@ from infrastructure.model import OutingModel
 from infrastructure.extension import db_session
 from infrastructure.util.random_key import random_key_generate
 from infrastructure.mapper.outing_repository_mapper import create_outing_mapper, get_outing_mapper, get_outings_mapper
-from infrastructure.exception import OutingExist, NotFound, NotApprovedByParents, AlreadyApprovedByParents
+from infrastructure.exception import OutingExist, NotFound, NotApprovedByParents, AlreadyApprovedByParents, StillOut
 from infrastructure.util.redis_service import get_oid_by_parents_outing_code, save_parents_outing_code, delete_outing_code
 
 from domain.repository.outing_repository import OutingRepository
@@ -86,4 +86,13 @@ class OutingRepositoryImpl(OutingRepository):
         if not outing.status == "1": raise NotApprovedByParents
 
         outing.status = "-2"
+        db_session.commit()
+
+    @classmethod
+    def certify_by_outing_for_teacher(cls, oid) -> None:
+        outing = db_session.query(OutingModel).filter(OutingModel.uuid == func.binary(oid)).first()
+
+        if not outing.status == "4": raise StillOut
+
+        outing.status = "5"
         db_session.commit()
