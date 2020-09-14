c0e9c0c14f9e644956498672c6aa25105a47cbbb
Maly <migsking@naver.com>
2020-09-14 13:05:06 +0900

    [ADD] <infra../model/outing_model.py> Add the enum "-2", "-1" and descrp

--- infrastructure/model/outing_model.py
+++ infrastructure/model/outing_model.py
@@ -8,7 +8,8 @@ class OutingModel(Base):
 
     uuid = Column(String(20), primary_key=True)
     student_uuid = Column(String(18), nullable=False)
-    status = Column(Enum("0", "1", "2", "3", "4", "5"), nullable=False)
+    status = Column(Enum("-2", "-1", "0", "1", "2", "3", "4", "5"), nullable=False)
+    # 0: 외출증 생성 , 1: 학부모 승인, -1: 학부모 거부, 2: 담임 확인, -2: 담임 거부, 3: 외출, 4: 외출 종료, 5: 사후제출
     situation = Column(Enum("NORMAL", "EMERGENCY"), nullable=False)
     date = Column(DateTime, nullable=False)
     start_time = Column(String(4), nullable=False)
