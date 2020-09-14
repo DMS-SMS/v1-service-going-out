de7f8facfb74e7766e5962849f4043f3623e7b57
Maly <migsking@naver.com>
2020-09-14 18:10:56 +0900

    [ADD] <application/mapper/outing_mapper.py> Create mapper for teacher

--- application/mapper/outing_mapper.py
+++ application/mapper/outing_mapper.py
@@ -1,7 +1,15 @@
 from typing import List
 
 from domain.entity.outing import Outing
-from proto.python.outing.outing_student_pb2 import StudentOuting as ProtoOuting
+
+from domain.repository.student_repository import StudentRepository
+from domain.repository.outing_repository import OutingRepository
+
+from infrastructure.repository.student_repository_impl import StudentRepositoryImpl
+from infrastructure.repository.outing_repository_impl import OutingRepositoryImpl
+
+from proto.python.outing.outing_student_pb2 import StudentOuting
+from proto.python.outing.outing_teacher_pb2 import Outing as TeacherOuting
 
 
 def create_outing_mapper(request):
@@ -15,11 +23,12 @@ def create_outing_mapper(request):
         reason=request.reason)
     return entity
 
-def get_outings_mapper(outing_entities: List["Outing"]) -> List["ProtoOuting"]:
+def get_outings_mapper(outing_entities: List["Outing"]) -> List["StudentOuting"]:
     outings = list()
 
     for outing_entity in outing_entities:
-        outing = ProtoOuting()
+        outing = StudentOuting()
+
         outing.place = outing_entity._place
         outing.reason = outing_entity._reason
         outing.date = outing_entity._date
@@ -27,6 +36,36 @@ def get_outings_mapper(outing_entities: List["Outing"]) -> List["ProtoOuting"]:
         outing.end_time = outing_entity._end_time
         outing.situation = outing_entity._situation
         outing.status = outing_entity._status
+
+        outings.append(outing)
+
+    return outings
+
+def get_outings_for_teacher_mapper(outing_entities: List["Outing"]) -> List["TeacherOuting"]:
+    outings = list()
+    student_repository: StudentRepository = StudentRepositoryImpl()
+    outing_repository: OutingRepository = OutingRepositoryImpl()
+
+    for outing_entity in outing_entities:
+        outing = TeacherOuting()
+        student = student_repository.get_student_by_uuid(outing_entity._student_uuid)
+        is_late = outing_repository.get_is_late(outing_entity._outing_uuid)
+
+        outing.name = student._name
+        outing.grade = student._grade
+        outing.class_ = student._class
+        outing.number = student._student_number
+
+        outing.place = outing_entity._place
+        outing.reason = outing_entity._reason
+        outing.date = outing_entity._date
+        outing.start_time = outing_entity._start_time
+        outing.end_time = outing_entity._end_time
+        outing.situation = outing_entity._situation
+        outing.status = outing_entity._status
+
+        if is_late is not None:
+            outing.is_late = is_late
 
         outings.append(outing)
 
