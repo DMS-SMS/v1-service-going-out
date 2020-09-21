252035bce6206032d0b5fff4a40aeaf0d1768586
Maly <migsking@naver.com>
2020-09-22 00:31:03 +0900

    [ADD] <presentation/config> add config

--- presentation/config/__init__.py
+++ presentation/config/__init__.py
@@ -15,18 +15,31 @@ class gRPCAppConfig:
 
 class ConsulConfig:
     def __init__(self):
-        self._host = "127.0.0.1"
+        self._host = "10.156.145.137"
         self._port = 8500
-        self._token = "12345"
+        self._service_host = None
+        self._service_port = None
+        self._service_id = "DMS.DSM.v1.service.outing"
+        self._service_name = "DMS.DSM.v1.service.outing"
+        self._token = "temporary_token"
 
     @property
-    def host(self):
-        return self._host
+    def host(self): return self._host
 
     @property
-    def port(self):
-        return self._port
+    def port(self): return self._port
 
     @property
-    def token(self):
-        return self._token
+    def service_host(self): return self._service_host
+
+    @property
+    def service_port(self): return self._service_port
+
+    @property
+    def service_id(self): return self._service_id
+
+    @property
+    def service_name(self): return self._service_name
+
+    @property
+    def token(self): return self._token
