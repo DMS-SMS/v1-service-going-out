dffcbecd80694b800d6c9a1ec71c651580b05cfd
Maly <migsking@naver.com>
2020-09-22 01:28:32 +0900

    [ADD] <presentation/extensions/consul_service.py> Check 관련된 것 추가

--- presentation/extensions/consul_service.py
+++ presentation/extensions/consul_service.py
@@ -1,4 +1,5 @@
 from consul import Consul
+from consul import Check
 
 from presentation.config import ConsulConfig
 
@@ -10,17 +11,25 @@ def create_consul():
         port=consul_config.port
     )
 
-    return consul.Agent(consul).Service(consul)
+    return consul.Agent(consul).Service(consul), consul.Agent(consul).Check(consul)
 
 
-def register_consul(consul):
+def register_consul(consul_service, consul_check):
     consul_config = ConsulConfig()
-    consul.register(name=consul_config.service_name,
+    consul_service.register(name=consul_config.service_name,
                     service_id=consul_config.service_id,
                     address=consul_config.service_host,
                     port=consul_config.service_port,
                     token=consul_config.token)
 
-def deregister_consul(consul):
+    consul_check.register(name="service 'DMS.SMS.v1.service.outing' check",
+                          check=Check.ttl("10s"),
+                          check_id = "TestCheckId",
+                          service_id = consul_config.service_id,
+                          token=consul_config.token
+    )
+
+def deregister_consul(consul_service, consul_check):
     consul_config = ConsulConfig()
-    consul.deregister(consul_config.service_id)
+    consul_service.deregister(consul_config.service_id)
+    consul_check.deregister("TestCheckId")
