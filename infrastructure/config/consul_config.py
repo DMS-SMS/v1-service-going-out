76559075d49e60b619353eb5215339bf4267e03e
Maly <migsking@naver.com>
2020-10-02 00:56:51 +0900

    [CREATE] Consul Config

--- infrastructure/config/consul_config.py
+++ infrastructure/config/consul_config.py
@@ -1,21 +1,6 @@
-class gRPCAppConfig:
-    def __init__(self):
-        self._max_workers = 10
-        self._host = "0.0.0.0"
-        self._port = 50051
-
-    @property
-    def max_workers(self):
-        return self._max_workers
-
-    @property
-    def address(self):
-        return str(self._host + ":" + str(self._port))
-
-
 class ConsulConfig:
     def __init__(self):
-        self._host = "10.156.145.137"
+        self._host = "127.0.0.1"
         self._port = 8500
 
         self._service_host = None
