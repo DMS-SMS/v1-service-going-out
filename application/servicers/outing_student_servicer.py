5e979b110f11fbf47b7113b509a177379c1f4af4
Maly <migsking@naver.com>
2020-10-02 01:46:38 +0900

    [ADD] DI student

--- application/servicers/outing_student_servicer.py
+++ application/servicers/outing_student_servicer.py
@@ -1,8 +1,14 @@
 from application.service.student_outing_service import StudentOutingService
+from domain.usecase.create_outing_usecase import CreateOutingUseCase
+from domain.usecase.finish_go_out_usecase import FinishGoOutUseCase
+from domain.usecase.get_card_usecase import GetCardUseCase
+from domain.usecase.get_my_outings_usecase import GetMyOutingsUseCase
+from domain.usecase.get_outing_inform_usecase import GetOutingInformUseCase
+from domain.usecase.go_out_usecase import GoOutUseCase
 
 from infrastructure.repository import OutingRepositoryImpl, StudentRepositoryImpl
 from infrastructure.service.outing_domain_service_impl import OutingDomainServiceImpl
-from infrastructure.service.sms_service import SMSService
+from infrastructure.service.sms_service_impl import SMSServiceImpl
 
 from proto.python.outing import outing_student_pb2_grpc
 
@@ -11,16 +17,34 @@ class StudentOutingServicer(outing_student_pb2_grpc.OutingStudentServicer):
     def __init__(self):
         self.outing_repository = OutingRepositoryImpl()
         self.student_repository = StudentRepositoryImpl()
-
         self.outing_domain_service = OutingDomainServiceImpl()
-
-        self.sms_service = SMSService()
+        self.sms_service = SMSServiceImpl()
 
         self.service = StudentOutingService(
-            outing_repository=self.outing_repository,
-            student_repository=self.student_repository,
-            outing_domain_service=self.outing_domain_service,
-            sms_service=self.sms_service,
+            create_outing_usecase=CreateOutingUseCase(
+                self.outing_repository,
+                self.student_repository,
+                self.sms_service,
+            ),
+            get_my_outings_usecase=GetMyOutingsUseCase(
+                self.outing_repository,
+                self.outing_domain_service
+            ),
+            get_outing_inform_usecase=GetOutingInformUseCase(
+                self.outing_repository,
+                self.outing_domain_service
+            ),
+            get_card_usecase=GetCardUseCase(
+                self.outing_repository,
+                self.student_repository,
+                self.outing_domain_service
+            ),
+            go_out_usecase=GoOutUseCase(
+                self.outing_repository
+            ),
+            finish_go_out_usecase=FinishGoOutUseCase(
+                self.outing_repository
+            )
         )
 
     def CreateOuting(self, request, context):
