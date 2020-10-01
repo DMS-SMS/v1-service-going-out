6b1f3a8984ad70f278c43fbc5071e9d7431ede69
Maly <migsking@naver.com>
2020-10-02 01:23:12 +0900

    [MODIFY] teacher usecase 사용

--- application/service/teacher_outing_service.py
+++ application/service/teacher_outing_service.py
@@ -1,44 +1,47 @@
+from domain.usecase.approve_outing_teacher_usecase import ApproveOutingTeacherUseCase
+from domain.usecase.certify_outing_usecase import CertifyOutingUseCase
+from domain.usecase.get_outings_with_filter_usecase import GetOutingsWithFilterUseCase
+from domain.usecase.reject_outing_teacher_usecase import RejectOutingTeacherUseCase
 from proto.python.outing import outing_teacher_pb2 as proto
 
 from application.decorator.error_handling import error_handling
 from application.mapper.outing_mapper import get_outings_for_teacher_mapper
 
-from domain.repository.outing_repository import OutingRepository
-from domain.service.outing_domain_service import OutingDomainService
-
-
 class TeacherOutingService:
-    def __init__(self, outing_repository, outing_domain_service):
-        self.outing_repository: OutingRepository = outing_repository
+    def __init__(
+            self,
+            approve_outing_teacher_usecase,
+            reject_outing_teacher_usecase,
+            certify_outing_usecase,
+            get_outings_with_filter_usecase
+    ):
+        self.approve_outing_teacher_usecase: ApproveOutingTeacherUseCase = approve_outing_teacher_usecase
+        self.reject_outing_teacher_usecase: RejectOutingTeacherUseCase = reject_outing_teacher_usecase
+        self.certify_outing_usecase: CertifyOutingUseCase = certify_outing_usecase
+        self.get_outings_with_filter_usecase: GetOutingsWithFilterUseCase = get_outings_with_filter_usecase
 
-        self.outing_domain_service: OutingDomainService = outing_domain_service
 
     @error_handling(proto.ConfirmOutingResponse)
     def approve_outing(self, request):
-        self.outing_repository.approve_by_outing_for_teacher(request.oid)
+        self.approve_outing_teacher_usecase.run(request.oid)
         return proto.ConfirmOutingResponse(status=200)
 
     @error_handling(proto.ConfirmOutingResponse)
     def reject_outing(self, request):
-        self.outing_repository.reject_by_outing_for_teacher(request.oid)
+        self.reject_outing_teacher_usecase.run(request.oid)
         return proto.ConfirmOutingResponse(status=200)
 
     @error_handling(proto.ConfirmOutingResponse)
     def certify_outing(self, request):
-        self.outing_repository.certify_by_outing_for_teacher(request.oid)
+        self.certify_outing_usecase.run(request.oid)
         return proto.ConfirmOutingResponse(status=200)
 
     @error_handling(proto.OutingResponse)
     def get_outings_with_filter(self, request):
-        response = proto.OutingResponse()
-        outings = self.outing_domain_service.paging_outings(
-            self.outing_repository.get_outings_with_filter(
-                request.status, request.grade, request.class_
-            ),
-            request.start,
-            request.count,
-        )
+        outings = self.get_outings_with_filter_usecase.run(
+            request.status, request.grade, request.class_, request.start, request.count)
 
+        response = proto.OutingResponse()
         response.status = 200
         response.outing.extend(get_outings_for_teacher_mapper(outings))
 
