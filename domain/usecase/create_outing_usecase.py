6efd2005d2021be844475bb78762d1f55987e46f
Maly <migsking@naver.com>
2020-10-02 01:23:32 +0900

    [MODIFY] 스네이크로 통일

--- domain/usecase/create_outing_usecase.py
+++ domain/usecase/create_outing_usecase.py
@@ -9,6 +9,8 @@ class CreateOutingUseCase:
         self.sms_service: SMSService = sms_service
 
     def run(self, entity):
-        oid: str = self.outing_repository.save_and_get_oid(entity)
-        o_code: str = self.outing_repository.set_and_get_parents_outing_code(oid)
-        self.sms_service.send_to_parents(oid, o_code)
+        o_id: str = self.outing_repository.save_and_get_oid(entity)
+        o_code: str = self.outing_repository.set_and_get_parents_outing_code(o_id)
+        self.sms_service.send_to_parents(o_id, o_code)
+
+        return o_id
