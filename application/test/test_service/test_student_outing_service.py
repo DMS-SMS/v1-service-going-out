d09ccfee330d8a43a7841119270ccc77b123dab5
Maly <migsking@naver.com>
2020-10-02 20:24:38 +0900

    [MODIFY] Mock Object DI

--- application/test/test_service/test_student_outing_service.py
+++ application/test/test_service/test_student_outing_service.py
@@ -1,5 +1,11 @@
 from unittest import TestCase
 
+from domain.usecase.create_outing_usecase import CreateOutingUseCase
+from domain.usecase.finish_go_out_usecase import FinishGoOutUseCase
+from domain.usecase.get_card_usecase import GetCardUseCase
+from domain.usecase.get_my_outings_usecase import GetMyOutingsUseCase
+from domain.usecase.get_outing_inform_usecase import GetOutingInformUseCase
+from domain.usecase.go_out_usecase import GoOutUseCase
 from proto.python.outing import outing_student_pb2
 
 from application.service.student_outing_service import StudentOutingService
@@ -13,11 +19,36 @@ from application.test.test_service.mock import (
 
 class TestStudentOutingService(TestCase):
     def setUp(self) -> None:
+        self.outing_repository = MockOutingRepository()
+        self.student_repository = MockStudentRepository()
+        self.outing_domain_service = MockOutingDomainService()
+        self.sms_service = MockSMSService()
+
         self.service = StudentOutingService(
-            outing_repository=MockOutingRepository(),
-            student_repository=MockStudentRepository(),
-            outing_domain_service=MockOutingDomainService(),
-            sms_service=MockSMSService(),
+            create_outing_usecase=CreateOutingUseCase(
+                self.outing_repository,
+                self.student_repository,
+                self.sms_service,
+            ),
+            get_my_outings_usecase=GetMyOutingsUseCase(
+                self.outing_repository,
+                self.outing_domain_service
+            ),
+            get_outing_inform_usecase=GetOutingInformUseCase(
+                self.outing_repository,
+                self.outing_domain_service
+            ),
+            get_card_usecase=GetCardUseCase(
+                self.outing_repository,
+                self.student_repository,
+                self.outing_domain_service
+            ),
+            go_out_usecase=GoOutUseCase(
+                self.outing_repository
+            ),
+            finish_go_out_usecase=FinishGoOutUseCase(
+                self.outing_repository
+            )
         )
         self.proto = outing_student_pb2
 
@@ -90,7 +121,7 @@ class TestStudentOutingService(TestCase):
             o_status="0",
             name="스윙스",
             grade=1,
-            class_=1,
+            group=1,
             number=11,
             profile_image_uri="/swings-pork-cutlet",
         )
