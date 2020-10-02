1ccedbe13c247d721e1861a79bcbf6dc236a91d8
Maly <migsking@naver.com>
2020-10-02 15:26:37 +0900

    [CREATE] db init 처리

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,13 @@
 import grpc
 from concurrent import futures
 
+from infrastructure.extension import Base, engine
+from infrastructure.model import OutingModel
 from infrastructure.config.grpc_config import gRPCAppConfig
 from application.servicers import register_outing_servicers
 
+def register_db():
+    Base.metadata.create_all(engine)
 
 def register_servicers(app):
     register_outing_servicers(app)
@@ -14,6 +18,7 @@ def create_app():
     app = grpc.server(futures.ThreadPoolExecutor(max_workers=config.max_workers))
     app.add_insecure_port(config.address)
 
+    register_db()
     register_servicers(app)
 
     return app
