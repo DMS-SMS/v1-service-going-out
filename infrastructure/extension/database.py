ad5ad7479bd02f873bdffd8a806fc27e77ad6032
Maly <migsking@naver.com>
2020-10-02 19:34:17 +0900

    [MODIFY] config 파일 변경

--- infrastructure/extension/database.py
+++ infrastructure/extension/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from infrastructure.config import DatabaseConfig
+from infrastructure.config.database_config import DatabaseConfig
 
 
 def register_db():
