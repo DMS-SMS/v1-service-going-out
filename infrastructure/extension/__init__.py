b91b8b20a2a256ce19df9c283ea881399fbd58e6
Maly <migsking@naver.com>
2020-10-02 19:33:52 +0900

    [MODIFY] Extension 순서 변경

--- infrastructure/extension/__init__.py
+++ infrastructure/extension/__init__.py
@@ -1,8 +1,8 @@
-from infrastructure.extension.database import register_db
-from infrastructure.extension.redis_register import register_redis
 from infrastructure.extension.consul_service import ConsulService
-
-
-db_session, Base, engine = register_db()
-redis = register_redis()
 consul = ConsulService()
+
+from infrastructure.extension.database import register_db
+db_session, Base, engine = register_db()
+
+from infrastructure.extension.redis_register import register_redis
+redis = register_redis()
