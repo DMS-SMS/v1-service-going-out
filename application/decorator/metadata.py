0829ec8d84fbb4b43f507f0a02194319ed28d096
Maly <migsking@naver.com>
2020-10-06 17:19:26 +0900

    [CREATE] metadata용 decorator 추가

--- application/decorator/metadata.py
+++ application/decorator/metadata.py
@@ -1,17 +1,27 @@
 from functools import wraps
+from infrastructure.extension import open_tracing
+from infrastructure.open_tracing import openTracing
 
 
-def x_request_id(fn):
+
+def jagger_enable(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
+        flag = 0
         context = args[2]
+        parents_span = str()
+        x_request_id = str()
         for k, v in context.invocation_metadata():
+            if flag == 2: break
             if k == "x-request-id":
-                context.set_trailing_metadata((
-                    ('x-request-id', v.encode()),
-                ))
-                break
+                x_request_id = v
+                flag += 1
+            if k == "span-context":
+                parents_span = v
+                flag += 1
+        else: raise Exception
 
-        return fn(*args, **kwargs)
+        open_tracing_service = openTracing(open_tracing.tracer, parents_span, x_request_id)
+        return open_tracing_service.start_active_span(fn, *args, **kwargs)
+
     return wrapper
-
