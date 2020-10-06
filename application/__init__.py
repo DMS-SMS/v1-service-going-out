41c0cf44c1436b8b72976d8531f2a708734e381e
Maly <migsking@naver.com>
2020-10-06 17:19:05 +0900

    [MODIFY] Application class 로 수정

--- application/__init__.py
+++ application/__init__.py
@@ -2,22 +2,31 @@ import grpc
 from concurrent import futures
 
 from infrastructure.extension import Base, engine
-from infrastructure.config.grpc_config import gRPCAppConfig
 from application.servicers import register_outing_servicers
 
-def register_db():
-    Base.metadata.create_all(engine)
 
-def register_servicers(app):
-    register_outing_servicers(app)
+class gRPCApplication:
+    def __init__(self, config, consul):
+        self._config = config
+        self._app = grpc.server(futures.ThreadPoolExecutor(max_workers=config.max_workers))
+        self._consul = consul
 
+        self._app.add_insecure_port(config.address)
+        self.register_db()
+        self.register_servicers()
 
-def create_app():
-    config = gRPCAppConfig()
-    app = grpc.server(futures.ThreadPoolExecutor(max_workers=config.max_workers))
-    app.add_insecure_port(config.address)
+    def register_db(self):
+        Base.metadata.create_all(engine)
 
-    register_db()
-    register_servicers(app)
+    def register_servicers(self):
+        register_outing_servicers(self._app)
 
-    return app
+    def serve(self):
+        try:
+            self._app.start()
+            self._consul.register_consul()
+            print("* gRPC Application is served")
+            self._app.wait_for_termination()
+        except:
+            self._consul.deregister_consul()
+            print("* gRPC Application is down")
