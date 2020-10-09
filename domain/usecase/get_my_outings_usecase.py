967bb968bd3eed049455b8db0e88acc80a1e390a
Maly <migsking@naver.com>
2020-10-09 15:28:08 +0900

    [MODIFY] #38 서비스 분리

--- domain/usecase/get_my_outings_usecase.py
+++ domain/usecase/get_my_outings_usecase.py
@@ -1,17 +1,19 @@
-from domain.repository import OutingRepository, StudentRepository
-from domain.service.outing_domain_service import OutingDomainService
+from domain.repository import OutingRepository
+from domain.service.uuid_service import uuidService
+from domain.service.paging_service import pagingService
 
 
 class GetMyOutingsUseCase:
-    def __init__(self, outing_repository, outing_domain_service):
+    def __init__(self, outing_repository, paging_service, uuid_service):
         self.outing_repository: OutingRepository = outing_repository
-        self.outing_domain_service: OutingDomainService = outing_domain_service
+        self.paging_service: pagingService = paging_service
+        self.uuid_service: uuidService = uuid_service
 
 
     def run(self, uuid, s_id, start, count):
-        self.outing_domain_service.compare_uuid_and_sid(uuid, s_id)
+        self.uuid_service.compare_uuid_and_sid(uuid, s_id)
 
-        return self.outing_domain_service.paging_outings(
+        return self.paging_service.paging_outings(
             self.outing_repository.get_outings_by_student_id(s_id),
             start,
             count,
