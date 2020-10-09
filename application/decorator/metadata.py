ef9d46cbc2a4ef315507c4fe70e51969e09431dd
Maly <migsking@naver.com>
2020-10-09 17:04:44 +0900

    [MODIFY] #48 Metadata Exception 처리

--- application/decorator/metadata.py
+++ application/decorator/metadata.py
@@ -2,6 +2,10 @@ from functools import wraps
 from infrastructure.open_tracing import open_tracing
 from infrastructure.open_tracing.open_tracing_handler import OpenTracing
 
+from domain.exception.not_proxy_auth import NotProxyAuth
+from domain.exception.not_proxy_auth.bad_span_context_exception import BadSpanContext
+from domain.exception.not_proxy_auth.bad_x_request_id_exception import BadXRequestId
+
 
 
 def jagger_enable(fn):
@@ -19,7 +23,10 @@ def jagger_enable(fn):
             if k == "span-context":
                 parents_span = v
                 flag += 1
-        else: raise Exception
+        else:
+            if not x_request_id: raise BadXRequestId
+            elif not parents_span: raise BadSpanContext
+            else: raise NotProxyAuth
 
         open_tracing_service = OpenTracing(open_tracing.tracer, parents_span, x_request_id)
         return open_tracing_service.start_active_span(fn, *args, **kwargs)
