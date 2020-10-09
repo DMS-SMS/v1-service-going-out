a737607a3f1441b420fff6273b6bf9643d111553
Maly <migsking@naver.com>
2020-10-09 15:24:18 +0900

    [MODIFY] #38 서비스 분리로 인한 경로 변경

--- application/servicers/outing_student_servicer.py
+++ application/servicers/outing_student_servicer.py
@@ -8,9 +8,11 @@ from domain.usecase.get_my_outings_usecase import GetMyOutingsUseCase
 from domain.usecase.get_outing_inform_usecase import GetOutingInformUseCase
 from domain.usecase.go_out_usecase import GoOutUseCase
 
-from infrastructure.repository import OutingRepositoryImpl, StudentRepositoryImpl
-from infrastructure.service.outing_domain_service_impl import OutingDomainServiceImpl
-from infrastructure.service.sms_service_impl import SMSServiceImpl
+from infrastructure.implementation.repository.outing_repository_impl import OutingRepositoryImpl
+from infrastructure.implementation.repository.student_repository_impl import StudentRepositoryImpl
+from infrastructure.implementation.service.paging_service_impl import pagingServiceImpl
+from infrastructure.implementation.service.uuid_service_impl import uuidServiceImpl
+from infrastructure.implementation.service.sms_service_impl import SMSServiceImpl
 
 from proto.python.outing import outing_student_pb2_grpc
 
@@ -19,7 +21,8 @@ class StudentOutingServicer(outing_student_pb2_grpc.OutingStudentServicer):
     def __init__(self):
         self.outing_repository = OutingRepositoryImpl()
         self.student_repository = StudentRepositoryImpl()
-        self.outing_domain_service = OutingDomainServiceImpl()
+        self.paging_service = pagingServiceImpl()
+        self.uuid_service = uuidServiceImpl()
         self.sms_service = SMSServiceImpl()
 
         self.service = StudentOutingService(
@@ -30,16 +33,17 @@ class StudentOutingServicer(outing_student_pb2_grpc.OutingStudentServicer):
             ),
             get_my_outings_usecase=GetMyOutingsUseCase(
                 self.outing_repository,
-                self.outing_domain_service
+                self.paging_service,
+                self.uuid_service
             ),
             get_outing_inform_usecase=GetOutingInformUseCase(
                 self.outing_repository,
-                self.outing_domain_service
+                self.uuid_service
             ),
             get_card_usecase=GetCardUseCase(
                 self.outing_repository,
                 self.student_repository,
-                self.outing_domain_service
+                self.uuid_service
             ),
             go_out_usecase=GoOutUseCase(
                 self.outing_repository
