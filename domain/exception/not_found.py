c3c368362eeae1323e2b12d7e1a0267151eaca19
Maly <migsking@naver.com>
2020-10-09 17:18:59 +0900

    [MODIFY] #48 Not Found 삭제

--- domain/exception/not_found.py
+++
@@ -1,8 +0,0 @@
-from domain.exception.business_exception import BusinessException
-
-
-class NotFound(BusinessException):
-    def __init__(self):
-        self.status: int = 404
-        self.code: int = -1001
-        self.msg: str = "NotFound"
