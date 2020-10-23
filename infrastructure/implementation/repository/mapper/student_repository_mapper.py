fb6d819a856d960ca1c342b09e387517473d72d4
Maly <migsking@naver.com>
2020-10-23 18:11:50 +0900

    [MODIFY] #54 Repository mapper 이름 변경

--- infrastructure/implementation/repository/mapper/student_repository_mapper.py
+++ infrastructure/implementation/repository/mapper/student_repository_mapper.py
@@ -1,15 +1,19 @@
+from typing import Optional
+
 from proto.python.auth import auth_student_pb2
 
 from domain.entity.student import Student
 
 
-def get_student_mapper(student_uuid, student_model: auth_student_pb2.GetStudentInformWithUUIDResponse) -> Student:
+def get_student_mapper(
+        student_uuid, student_proto: Optional["auth_student_pb2.GetStudentInformWithUUIDResponse"]
+    ) -> Optional["Student"]:
     return Student(
         student_uuid=student_uuid,
-        grade=student_model.Grade,
-        group=student_model.Group,
-        student_number=student_model.StudentNumber,
-        name=student_model.Name,
-        phone_number=student_model.PhoneNumber,
-        profile_image_uri=student_model.ImageURI,
+        grade=student_proto.Grade,
+        group=student_proto.Group,
+        student_number=student_proto.StudentNumber,
+        name=student_proto.Name,
+        phone_number=student_proto.PhoneNumber,
+        profile_image_uri=student_proto.ImageURI,
     )
