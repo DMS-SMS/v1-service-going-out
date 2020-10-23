2882e2458e4e68ec03ccdd61653c715a4cc5c65a
Maly <migsking@naver.com>
2020-10-23 18:09:52 +0900

    [MODIFY] #53 uuid 서비스 재 정의 및 구현

--- infrastructure/implementation/service/uuid_service_impl.py
+++ infrastructure/implementation/service/uuid_service_impl.py
@@ -1,12 +1,22 @@
 from domain.service.uuid_service import UuidService
+from infrastructure.implementation.repository.confirm_code_repository_impl import ConfirmCodeRepositoryImpl
+from infrastructure.implementation.repository.outing_repository_impl import OutingRepositoryImpl
 
-from infrastructure.util.string_util import compare_element
-
-from domain.exception import Unauthorized
+from infrastructure.util.random_key import generate_confirm_code, generate_outing_uuid
 
 
 class UuidServiceImpl(UuidService):
-    @classmethod
-    def compare_uuid_and_sid(cls, uuid, sid):
-        if not compare_element(uuid, sid):
-            raise Unauthorized
+    def __init__(self):
+        self.outing_repository = OutingRepositoryImpl()
+        self.confirm_code_repository = ConfirmCodeRepositoryImpl()
+
+    def generate_outing_uuid(self) -> str:
+        uuid = generate_outing_uuid()
+        while self.outing_repository.find_by_id(uuid): uuid = generate_outing_uuid()
+        return uuid
+
+    def generate_confirm_code(self) -> str:
+        code = generate_confirm_code()
+        while self.confirm_code_repository.find_by_code(code): code = generate_confirm_code()
+        return code
+
