3231856062a72e4cbc0f3daad8fc99e7adaef366
Maly <migsking@naver.com>
2020-10-23 18:17:22 +0900

    [MODIFY] #53 하나로 통일

--- domain/exception/business_exception/not_been_approved.py
+++ domain/exception/business_exception/not_been_approved.py
@@ -3,22 +3,8 @@ from const.code.python import outing
 from domain.exception.business_exception import BusinessException
 
 
-class NotApprovedByParents(BusinessException):
-    def __init__(self):
-        self.status: int = int(HTTPStatus.CONFLICT)
-        self.code: int = outing.not_approved_by_parents
-        self.msg: str = "It has not been approved by parents yet"
-
-
-class AlreadyApprovedByTeacher(BusinessException):
-    def __init__(self):
-        self.status: int = int(HTTPStatus.CONFLICT)
-        self.code: int = outing.already_approved_by_teacher
-        self.msg: str = "It has been approved by teacher"
-
-
-class AlreadyApprovedByParents(BusinessException):
+class ConfirmFailed(BusinessException):
     def __init__(self):
         self.status: int = int(HTTPStatus.CONFLICT)
         self.code: int = outing.already_approved_by_parents
-        self.msg: str = "It has been approved by parents"
+        self.msg: str = "The request cannot be executed"
