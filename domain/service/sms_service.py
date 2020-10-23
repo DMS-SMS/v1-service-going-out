c7a7acfea6b0c63404a4078fde0c70c015481f92
Maly <migsking@naver.com>
2020-10-23 18:15:00 +0900

    [MODIFY] #53 x request id 인자 추가

--- domain/service/sms_service.py
+++ domain/service/sms_service.py
@@ -3,5 +3,5 @@ from abc import ABCMeta, abstractmethod
 
 class SMSService(metaclass=ABCMeta):
     @abstractmethod
-    def send_to_parents(self, oid: str, o_code: str):
+    def send_to_parents(self, oid: str, o_code: str, x_request_id):
         pass
