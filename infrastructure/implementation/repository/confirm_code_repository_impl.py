59a2d90a438ad2a5d506d4c3a014694504255ed0
Maly <migsking@naver.com>
2020-10-23 18:12:16 +0900

    [CREATE] #54 Confirm Code Repo 추가

---
+++ infrastructure/implementation/repository/confirm_code_repository_impl.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from domain.repository.confirm_code_repository import ConfirmCodeRepository
+from infrastructure.redis import RedisHandler
+
+
+class ConfirmCodeRepositoryImpl(ConfirmCodeRepository):
+    def __init__(self):
+        self.redis = RedisHandler()
+
+    def save(self, outing_uuid: str, confirm_code: str):
+        self.redis.save(confirm_code, outing_uuid)
+
+    def find_by_code(self, confirm_code: str) -> Optional["str"]:
+        return self.redis.find_by_key(confirm_code)
