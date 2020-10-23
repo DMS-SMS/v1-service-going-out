c6a11a51d969a3df8da9a61cacd69dc3b0df0a4d
Maly <migsking@naver.com>
2020-10-23 18:13:50 +0900

    [MODIFY] #55 UseCase 재 정의

--- domain/usecase/get_outing_inform_usecase.py
+++ domain/usecase/get_outing_inform_usecase.py
@@ -1,18 +1,21 @@
 from domain.entity import Outing
-from domain.repository import OutingRepository
+from domain.exception import Unauthorized, OutingNotFound
+from domain.repository.outing_repository import OutingRepository
+from domain.repository.student_repository import StudentRepository
 from domain.service.uuid_service import UuidService
 
 
 class GetOutingInformUseCase:
-    def __init__(self, outing_repository, uuid_service):
+    def __init__(self, outing_repository, student_repository, uuid_service):
         self.outing_repository: OutingRepository = outing_repository
+        self.student_repository: StudentRepository = student_repository
         self.uuid_service: UuidService = uuid_service
 
-    def run(self, uuid, o_id):
-        outing: Outing = self.outing_repository.get_outing_by_oid(o_id)
+    def run(self, uuid, outing_id, x_request_id):
+        if self.student_repository.find_by_uuid(uuid, x_request_id) is None: raise Unauthorized()
 
-        self.uuid_service.compare_uuid_and_sid(
-            uuid, outing._student_uuid
-        )
+        outing: Outing = self.outing_repository.find_by_id(outing_id)
+        if outing is None: raise OutingNotFound()
 
+        if outing.student_uuid != uuid: raise Unauthorized()
         return outing
