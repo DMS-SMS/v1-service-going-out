e07cddadce80581d527ae99589d8039c800c01b4
Maly <migsking@naver.com>
2020-10-25 14:57:35 +0900

    [ADD] #15 환경 변수 설정

--- infrastructure/config/grpc_config.py
+++ infrastructure/config/grpc_config.py
@@ -1,8 +1,11 @@
+from infrastructure.config.consul_config import ConsulConfig
+
+
 class gRPCAppConfig:
     def __init__(self):
         self._max_workers = 10
-        self._host = "0.0.0.0"
-        self._port = 50051
+        self._host = ConsulConfig.service_host
+        self._port = ConsulConfig.service_port
 
     @property
     def max_workers(self):
