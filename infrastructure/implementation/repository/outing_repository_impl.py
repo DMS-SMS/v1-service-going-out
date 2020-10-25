46cc1728bf7ba27a20f92d24e69e38e2dae5e94d
Maly <migsking@naver.com>
2020-10-25 14:58:22 +0900

    [ADD] #31

--- infrastructure/implementation/repository/outing_repository_impl.py
+++ infrastructure/implementation/repository/outing_repository_impl.py
@@ -7,28 +7,35 @@ from sqlalchemy import func, and_
 from domain.entity import Outing
 from domain.repository.outing_repository import OutingRepository
 from infrastructure.mysql import MySQLHandler
+from infrastructure.open_tracing import open_tracing
+from infrastructure.open_tracing.open_tracing_handler import trace_service
 
 
 class OutingRepositoryImpl(OutingRepository):
     def __init__(self):
         self.sql = MySQLHandler()
 
+    @trace_service("SQL (save)", open_tracing)
     def save(self, outing: Outing):
         self.sql.db_session.add(outing)
         self.sql.db_session.commit()
 
+    @trace_service("SQL (find)", open_tracing)
     def find_all_by_student_uuid(self, student_id):
         return self.sql.db_session.query(Outing).filter(Outing.student_uuid == func.binary(student_id)).all()
 
+    @trace_service("SQL (find)", open_tracing)
     def find_all_by_student_uuid_and_status(self, student_uuid, status) -> List["Outing"]:
         query = self.sql.db_session.query(Outing).filter(Outing.student_uuid == func.binary(student_uuid))
         if status: query = query.filter(Outing.status == status)
         return query.all()
 
+    @trace_service("SQL (find)", open_tracing)
     def find_by_id(self, id: str) -> Outing:
         return (self.sql.db_session.query(Outing)
                 .filter(Outing.outing_uuid == func.binary(id)).first())
 
+    @trace_service("SQL (find)", open_tracing)
     def find_by_student_uuid_and_end_time(self, student_uuid: str, time: float) -> Outing:
         return (self.sql.db_session.query(Outing)
                 .filter(and_(Outing.start_time <= datetime.datetime.fromtimestamp(time),
--- infrastructure/redis/redis_handler.py
+++ infrastructure/redis/redis_handler.py
@@ -3,6 +3,8 @@ from typing import Optional
 from redis import StrictRedis
 
 from infrastructure.config.cache_config import CacheConfig
+from infrastructure.open_tracing import open_tracing
+from infrastructure.open_tracing.open_tracing_handler import trace_service
 
 
 class RedisHandler:
@@ -19,9 +21,11 @@ class RedisHandler:
     def redis(self):
         return self._redis
 
+    @trace_service("Redis (save)", open_tracing)
     def save(self, key: str, value: str):
         self._redis.set(key, value)
 
+    @trace_service("Redis (find)", open_tracing)
     def find_by_key(self, key: str) -> Optional[str]:
         try:
             value = self._redis.get(key).decode()
@@ -29,5 +33,6 @@ class RedisHandler:
             value = None
         return value
 
+    @trace_service("Redis (delete)", open_tracing)
     def delete_by_key(self, key: str):
         self._redis.delete(key)
