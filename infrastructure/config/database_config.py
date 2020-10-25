29d61346174a6edefd1431475daaede377ebe23b
Maly <migsking@naver.com>
2020-10-25 17:05:16 +0900

    [FIX] #60 native password

--- infrastructure/config/database_config.py
+++ infrastructure/config/database_config.py
@@ -19,7 +19,7 @@ class DatabaseConfig:
     @property
     def address(self) -> str:
         return str(
-            f"{self._sql}://{self._user}:{self._password}@{self._host}/{self._db}?charset=utf8"
+            f"{self._sql}://{self._user}:{self._password}@{self._host}/{self._db}?charset=utf8&auth_plugin=mysql_native_password"
         )
 
     @property
