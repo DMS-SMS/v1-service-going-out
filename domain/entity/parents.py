da20a762bac1e80d3af74d8b4dc4efaf41251610
Maly <migsking@naver.com>
2020-12-07 13:07:43 +0900

    [CREATE] #80 Parents

---
+++ domain/entity/parents.py
@@ -0,0 +1,10 @@
+class Parents:
+    def __init__(
+        self,
+        uuid=None,
+        name=None,
+        phone_number=None,
+    ):
+        self._uuid: str = uuid
+        self._name: str = name
+        self._phone_number: str = phone_number
