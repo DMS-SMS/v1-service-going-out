e5dcf0b4e1fc330dcb3973bf8471fbb42d017a65
Maly <migsking@naver.com>
2020-12-07 16:22:14 +0900

    [ADD] #83 Create Repository

--- domain/repository/parents_repository.py
+++ domain/repository/parents_repository.py
@@ -7,3 +7,7 @@ class ParentsRepository(metaclass=ABCMeta):
     @abstractmethod
     def find_by_student_uuid(self, uuid: str, student_uuid: str, x_request_id: str) -> Optional["Parents"]:
         pass
+
+    @abstractmethod
+    def find_by_uuid(self, uuid:str, parent_uuid, x_request_id: str) -> Optional["Parents"]:
+        pass
