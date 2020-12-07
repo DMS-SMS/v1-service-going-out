3f10f68e5a99f87eb34e3fd180d364a037225576
Maly <migsking@naver.com>
2020-12-07 16:23:23 +0900

    [MODIFY] #83 add repository

--- infrastructure/implementation/repository/parents_repository_impl.py
+++ infrastructure/implementation/repository/parents_repository_impl.py
@@ -2,7 +2,8 @@ from typing import List, Optional
 
 from domain.entity.parents import Parents
 from domain.repository.parents_repository import ParentsRepository
-from infrastructure.implementation.repository.mapper.parents_repository_mapper import get_parents_mapper
+from infrastructure.implementation.repository.mapper.parents_repository_mapper import \
+    get_parents_by_student_uuid_mapper, get_parents_mapper
 from infrastructure.auth.auth_handler import AuthHandler
 
 
@@ -11,4 +12,7 @@ class ParentsRepositoryImpl(ParentsRepository):
         self.auth = AuthHandler()
 
     def find_by_student_uuid(self, uuid: str, student_uuid: str, x_request_id: str) -> Optional["Parents"]:
-        return get_parents_mapper(self.auth.get_parents_with_student_uuid(uuid, student_uuid, x_request_id))
+        return get_parents_by_student_uuid_mapper(self.auth.get_parents_with_student_uuid(uuid, student_uuid, x_request_id))
+
+    def find_by_uuid(self, uuid:str, parent_uuid, x_request_id: str) -> Optional["Parents"]:
+        return get_parents_mapper(uuid, self.auth.get_parents_inform(uuid, parent_uuid, x_request_id))
