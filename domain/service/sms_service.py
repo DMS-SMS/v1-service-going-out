65d74f5da197f8b5257a099f39c861cc6d487206
Maly <migsking@naver.com>
2020-12-07 11:02:04 +0900

    [MODIFY] #13 SMS Service interface

--- domain/service/sms_service.py
+++ domain/service/sms_service.py
@@ -3,5 +3,4 @@ from abc import ABCMeta, abstractmethod
 
 class SMSService(metaclass=ABCMeta):
     @abstractmethod
-    def send_to_parents(self, oid: str, o_code: str, x_request_id):
-        pass
+    def send(self, target_number: str, message: str, x_request_id): pass
