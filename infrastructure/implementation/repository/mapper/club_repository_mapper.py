4aa4f129a8e990838e2ea82ddf8cbe96c59e41a3
Maly <migsking@naver.com>
2020-12-08 15:09:19 +0900

    [MODIFY] #74 mapper

--- infrastructure/implementation/repository/mapper/club_repository_mapper.py
+++ infrastructure/implementation/repository/mapper/club_repository_mapper.py
@@ -3,8 +3,9 @@ from domain.entity.club import Club
 from proto.python.club import club_student_pb2
 
 def get_club_list_mapper(clubs_proto: club_student_pb2.GetClubInformsWithUUIDsResponse):
+    if clubs_proto == None: return []
     clubs = []
-    for club in clubs_proto.informs:
+    for club in clubs_proto.Informs:
         clubs.append(
             Club(
                 uuid=club.ClubUUID,
