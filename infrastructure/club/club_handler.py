10027190e627727250f385e7b4985e6f8edf1e72
Maly <migsking@naver.com>
2020-12-14 11:13:18 +0900

    [FIX] #92 Club service 실시간 discovery 하도록 modify

--- infrastructure/club/club_handler.py
+++ infrastructure/club/club_handler.py
@@ -12,24 +12,25 @@ from const.topic.python.service_names import club_service_name
 class ClubHandler:
     def __init__(self):
         self._consul = ConsulHandler()
-        self._address = self._consul.get_address(club_service_name)
-        self._channel = grpc.insecure_channel(self._address)
-        self._student_stub = club_student_pb2_grpc.ClubStudentStub(self._channel)
 
     @trace_service("Club Handler (get_club_informs_with_floor)", open_tracing)
     def get_club_informs_with_floor(self, uuid, floor, x_request_id):
-        self.metadata = (("x-request-id", x_request_id),
+        address = self._consul.get_address(club_service_name)
+        channel = grpc.insecure_channel(address)
+        student_stub = club_student_pb2_grpc.ClubStudentStub(channel)
+
+        metadata = (("x-request-id", x_request_id),
                          ("span-context", str(open_tracing.tracer.active_span).split()[0]))
 
-        club_response = self._student_stub.GetClubUUIDsWithFloor(club_student_pb2.GetClubUUIDsWithFloorRequest(
+        club_response = student_stub.GetClubUUIDsWithFloor(club_student_pb2.GetClubUUIDsWithFloorRequest(
             UUID=uuid,
             floor=str(floor)
-        ), metadata=self.metadata)
+        ), metadata=metadata)
 
-        response = self._student_stub.GetClubInformsWithUUIDs(club_student_pb2.GetClubInformsWithUUIDsRequest(
+        response = student_stub.GetClubInformsWithUUIDs(club_student_pb2.GetClubInformsWithUUIDsRequest(
             UUID=uuid,
             ClubUUIDs=club_response.ClubUUIDs
-        ), metadata=self.metadata)
+        ), metadata=metadata)
 
         if response.Status != 200: return None
 
