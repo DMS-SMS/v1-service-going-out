225f31b32a881625cfca8de9d505f2833fb57cd2
Maly <migsking@naver.com>
2020-12-14 16:38:27 +0900

    [FIX] #90 Connection fool

--- infrastructure/implementation/repository/outing_repository_impl.py
+++ infrastructure/implementation/repository/outing_repository_impl.py
@@ -18,27 +18,35 @@ class OutingRepositoryImpl(OutingRepository):
     @trace_service("SQL (save)", open_tracing)
     def save(self, outing: Outing):
         self.sql.db_session.add(outing)
-        self.sql.db_session.commit()
+        self.sql.db_session.close()
 
     @trace_service("SQL (find)", open_tracing)
     def find_all_by_student_uuid(self, student_id):
-        return self.sql.db_session.query(Outing)\
+        model = self.sql.db_session.query(Outing)\
             .filter(Outing.student_uuid == func.binary(student_id))\
             .order_by(Outing.end_time.desc()).all()
+        self.sql.db_session.close()
+        return model
 
     @trace_service("SQL (find)", open_tracing)
     def find_all_by_student_uuid_and_status(self, student_uuid, status) -> List["Outing"]:
         query = self.sql.db_session.query(Outing).filter(Outing.student_uuid == func.binary(student_uuid))
         if status: query = query.filter(Outing.status == status)
-        return query.all()
+        model = query.all()
+        self.sql.db_session.close()
+        return model
 
     @trace_service("SQL (find)", open_tracing)
     def find_by_id(self, id: str) -> Outing:
-        return (self.sql.db_session.query(Outing)
+        model = (self.sql.db_session.query(Outing)
                 .filter(Outing.outing_uuid == func.binary(id)).first())
+        self.sql.db_session.close()
+        return model
 
     @trace_service("SQL (find)", open_tracing)
     def find_by_student_uuid_and_end_time(self, student_uuid: str, time: float) -> Outing:
-        return (self.sql.db_session.query(Outing)
+        model = (self.sql.db_session.query(Outing)
                 .filter(and_(Outing.start_time <= datetime.datetime.fromtimestamp(time),
                              Outing.end_time >= datetime.datetime.fromtimestamp(time))).first())
+        self.sql.db_session.close()
+        return model
