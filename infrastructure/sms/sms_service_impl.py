170d3d5504847d715ed34451719d954426543301
Maly <migsking@naver.com>
2020-12-16 18:30:13 +0900

    [TEMPORARY] 임시 이메일 설정

--- infrastructure/sms/sms_service_impl.py
+++ infrastructure/sms/sms_service_impl.py
@@ -6,4 +6,19 @@ from infrastructure.open_tracing.open_tracing_handler import trace_service
 class SMSServiceImpl(SMSService):
     @trace_service("SMS (send)", open_tracing)
     def send(self, target_number: str, message: str, x_request_id):
-        pass
+        # Temporary
+        import os
+        import smtplib
+        from email.mime.text import MIMEText
+
+        smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('frametemporary123@gmail.com', os.getenv("EMAIL_PASSWORD"))
+
+        msg = MIMEText(message)
+        msg['Subject'] = 'SMS 테스트'
+        msg['To'] = 'dltjdqhr55@gmail.com'
+        smtp.sendmail('frametemporary123@gmail.com', 'dltjdqhr55@gmail.com', msg.as_string())
+
+        smtp.quit()
