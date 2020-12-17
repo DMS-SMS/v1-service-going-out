f648ea6a09e1fd764e14bb193a892d48067d25b2
Maly <migsking@naver.com>
2020-12-17 11:48:54 +0900

    [ADD] Regist the Service

--- application/servicers/outing_parents_servicer.py
+++ application/servicers/outing_parents_servicer.py
@@ -1,4 +1,7 @@
+from domain.usecase.get_account_by_uuid_usecase import GetAccountByUuidUseCase
+from domain.usecase.get_outing_by_o_code_usecase import GetOutingByOCodeUseCase
 from infrastructure.implementation.repository.confirm_code_repository_impl import ConfirmCodeRepositoryImpl
+from infrastructure.implementation.repository.student_repository_impl import StudentRepositoryImpl
 from service.parents_outing_service import ParentsOutingService
 from domain.usecase.approve_outing_usecase import ApproveOutingUseCase
 from domain.usecase.reject_outing_usecase import RejectOutingUseCase
@@ -12,6 +15,7 @@ from application.decorator.error_handling import error_handling
 class ParentsOutingServicer(outing_parents_pb2_grpc.OutingParentsServicer):
     def __init__(self):
         self.confirm_code_repository = ConfirmCodeRepositoryImpl()
+        self.student_repository = StudentRepositoryImpl()
         self.outing_repository = OutingRepositoryImpl()
         self.service = ParentsOutingService(
             approve_outing_usecase=ApproveOutingUseCase(
@@ -19,6 +23,13 @@ class ParentsOutingServicer(outing_parents_pb2_grpc.OutingParentsServicer):
                 self.confirm_code_repository
             ),
             reject_outing_usecase=RejectOutingUseCase(
+                self.outing_repository,
+                self.confirm_code_repository
+            ),
+            get_account_by_uuid_usecase=GetAccountByUuidUseCase(
+                self.student_repository
+            ),
+            get_outing_by_o_code_usecase=GetOutingByOCodeUseCase(
                 self.outing_repository,
                 self.confirm_code_repository
             )
@@ -33,3 +44,8 @@ class ParentsOutingServicer(outing_parents_pb2_grpc.OutingParentsServicer):
     @jagger_enable
     def RejectOutingByOCode(self, request, context):
         return self.service.reject_outing(request, context)
+
+    @error_handling(outing_parents_pb2.GetOutingByOCodeResponse)
+    @jagger_enable
+    def GetOutingByOCode(self, request, context):
+        return self.service.get_outing(request, context)
--- service/parents_outing_service.py
+++ service/parents_outing_service.py
@@ -1,11 +1,20 @@
+import time
+
+from domain.entity import Outing, Student
+from domain.usecase.get_account_by_uuid_usecase import GetAccountByUuidUseCase
+from domain.usecase.get_outing_by_o_code_usecase import GetOutingByOCodeUseCase
 from domain.usecase.reject_outing_usecase import RejectOutingUseCase
 from proto.python.outing import outing_parents_pb2 as proto
 from domain.usecase.approve_outing_usecase import ApproveOutingUseCase
+from service.util.get_x_request_id import get_x_request_id
+
 
 class ParentsOutingService:
-    def __init__(self, approve_outing_usecase, reject_outing_usecase):
+    def __init__(self, approve_outing_usecase, reject_outing_usecase, get_outing_by_o_code_usecase, get_account_by_uuid_usecase):
         self.approve_outing_usecase: ApproveOutingUseCase = approve_outing_usecase
         self.reject_outing_usecase: RejectOutingUseCase = reject_outing_usecase
+        self.get_outing_by_o_code_usecase : GetOutingByOCodeUseCase = get_outing_by_o_code_usecase
+        self.get_account_by_uuid_usecase: GetAccountByUuidUseCase = get_account_by_uuid_usecase
 
     def approve_outing(self, request, context):
         self.approve_outing_usecase.run(request.confirm_code)
@@ -14,3 +23,18 @@ class ParentsOutingService:
     def reject_outing(self, request, context):
         self.reject_outing_usecase.run(request.confirm_code)
         return proto.ConfirmOutingByOCodeResponse(status=200)
+
+    def get_outing(self, request, context):
+        outing: Outing = self.get_outing_by_o_code_usecase.run(request.confirm_code)
+        student: Student = self.get_account_by_uuid_usecase.run(outing.student_uuid, get_x_request_id(context))
+
+        return proto.GetOutingByOCodeResponse(
+            status = 200,
+            name=student._name,
+            outing_id=outing.outing_uuid,
+            start_time=int(time.mktime(outing.start_time.timetuple())),
+            end_time=int(time.mktime(outing.end_time.timetuple())),
+            place=outing.place,
+            reason=outing.reason,
+            situation=outing.situation
+        )
