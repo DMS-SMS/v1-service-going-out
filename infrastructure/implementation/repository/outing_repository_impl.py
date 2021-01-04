05a8168c899f881dd6bb48e1edbc7c570f243036
Maly <migsking@naver.com>
2021-01-04 10:40:11 +0900

    [FIX] 외출증 생성 시간 조절

--- infrastructure/implementation/repository/outing_repository_impl.py
+++ infrastructure/implementation/repository/outing_repository_impl.py
@@ -52,8 +52,8 @@ class OutingRepositoryImpl(OutingRepository):
         generated_time = datetime.datetime.fromtimestamp(time+32400)
         current_day = datetime.datetime(generated_time.year, generated_time.month, generated_time.day)
         model = (self.sql._db_session.query(Outing)
-                .filter(and_(Outing.start_time <= current_day,
-                             Outing.end_time >= current_day)).first())
+                .filter(and_(Outing.start_time >= current_day,
+                             Outing.end_time <= current_day)).first())
         self.sql._db_session.commit()
         self.sql._db_session.close()
         return model
